041d6a0a4f535ec7a1079b9244d46561838fb25f
Braian-Quintian <bquintianromero@gmail.com>
2023-05-22 17:41:29 -0500

    refactor(index): :hammer: The js file is formatted

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 const searchButton = document.getElementById('searchButton');
 
-searchButton.addEventListener('click', async function() {
-  const searched = document.getElementById('search').value;
-  const url = `https://real-time-product-search.p.rapidapi.com/search?q=${searched}&country=co&language=es`;
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'ef83921e6bmsh953b269570e564dp16eac6jsn64d0c9daeb77',
-      'X-RapidAPI-Host': 'real-time-product-search.p.rapidapi.com'
-    }
-  };
+searchButton.addEventListener('click', async function () {
+    const searched = document.getElementById('search').value;
+    const url = `https://real-time-product-search.p.rapidapi.com/search?q=${searched}&country=co&language=es`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'ef83921e6bmsh953b269570e564dp16eac6jsn64d0c9daeb77',
+            'X-RapidAPI-Host': 'real-time-product-search.p.rapidapi.com'
+        }
+    };
 
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
 });

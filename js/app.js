d0e672e00a8fd3bf6d8e669c79f2b37488ee95c0
Braian-Quintian <bquintianromero@gmail.com>
2023-05-22 18:45:58 -0500

    fix(index): :ambulance: fixed usage api

--- js/app.js
+++ js/app.js
@@ -2,18 +2,18 @@ const searchButton = document.getElementById('searchButton');
 
 searchButton.addEventListener('click', async function () {
     const searched = document.getElementById('search').value;
-    const url = `https://real-time-product-search.p.rapidapi.com/search?q=${searched}&country=co&language=es`;
+    const url = `https://amazon23.p.rapidapi.com/product-search?query=${searched}&country=US`;
     const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': 'ef83921e6bmsh953b269570e564dp16eac6jsn64d0c9daeb77',
-            'X-RapidAPI-Host': 'real-time-product-search.p.rapidapi.com'
+            'X-RapidAPI-Host': 'amazon23.p.rapidapi.com'
         }
     };
 
     try {
         const response = await fetch(url, options);
-        const result = await response.text();
+        const result = await response.json();
         console.log(result);
     } catch (error) {
         console.error(error);

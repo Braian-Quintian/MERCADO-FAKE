dc473aea96143a6188909d22cbf8fb115905d1cb
Braian-Quintian <bquintianromero@gmail.com>
2023-05-22 17:25:37 -0500

    feat(index): :sparkles: The search box is created
    The search box is created to look for the results with the API, IT SHOULD BE NOTED THAT IT IS ONLY A TEST

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const searched = document.getElementById('search').value;
+
+const url = `https://real-time-product-search.p.rapidapi.com/search?q=${searched}&country=co&language=es`;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ef83921e6bmsh953b269570e564dp16eac6jsn64d0c9daeb77',
+		'X-RapidAPI-Host': 'real-time-product-search.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}

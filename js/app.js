434283f035996c3919b48747a6b8308320afdc44
Braian-Quintian <bquintianromero@gmail.com>
2023-05-22 23:43:53 -0500

    perf: :sparkles: Nueva estructura en el js
    Se mejora la legibilidad del código y se proporcionan nuevas funciones

--- js/app.js
+++ js/app.js
@@ -21,34 +21,27 @@ searchButton.addEventListener('click', async function () {
         resultDiv.innerHTML = '';
 
         // Obtener los primeros 15 productos o menos si hay menos de 15
-        const productsToShow = result.result.slice(0, 15);
+        const productsToShow = result.result.slice(0, 50);
 
         // Recorrer los productos obtenidos
         productsToShow.forEach(product => {
             const { thumbnail, title, price, reviews } = product;
-            const { currency, current_price } = price;
             const { rating } = reviews;
 
             // Crear elementos HTML para mostrar la información
-            const productDiv = document.createElement('div');
-            productDiv.classList.add('product'); // Agregar la clase 'product' al div del producto
-
-            const thumbnailImg = document.createElement('img');
-            const titleP = document.createElement('p');
-            const priceP = document.createElement('p');
+            const productDiv = createProductDiv();
+            const thumbnailImg = createThumbnailImg(thumbnail);
+            const textDiv = createTextDiv();
+            const titleP = createTitleParagraph(title);
+            const priceP = createPriceParagraph(price);
             const ratingDiv = createRatingStars(rating);
-            productDiv.appendChild(ratingDiv);
 
-            thumbnailImg.src = thumbnail;
-            titleP.textContent = title;
-            priceP.textContent = `${currency} ${current_price}`;
-
-            // Agregar los elementos al div del producto
+            // Estructurar los elementos creados
             productDiv.appendChild(thumbnailImg);
-            productDiv.appendChild(titleP);
-            productDiv.appendChild(priceP);
-
-            // Agregar el div del producto al div principal de resultados
+            textDiv.appendChild(titleP);
+            textDiv.appendChild(priceP);
+            textDiv.appendChild(ratingDiv);
+            productDiv.appendChild(textDiv);
             resultDiv.appendChild(productDiv);
         });
     } catch (error) {
@@ -56,28 +49,75 @@ searchButton.addEventListener('click', async function () {
     }
 });
 
+function createProductDiv() {
+    const productDiv = document.createElement('div');
+    productDiv.classList.add('product');
+    return productDiv;
+}
+
+function createThumbnailImg(src) {
+    const thumbnailImg = document.createElement('img');
+    thumbnailImg.src = src;
+    return thumbnailImg;
+}
+
+function createTextDiv() {
+    const textDiv = document.createElement('div');
+    textDiv.classList.add('text');
+    return textDiv;
+}
+
+function createTitleParagraph(text) {
+    const titleP = document.createElement('p');
+    titleP.classList.add('title');
+    titleP.textContent = text;
+    return titleP;
+}
+
+function createPriceParagraph(price) {
+    const formattedPrice = formatPrice(price);
+    const priceP = document.createElement('p');
+    priceP.classList.add('price');
+    priceP.textContent = formattedPrice;
+    return priceP;
+}
+
 function createRatingStars(rating) {
     const fullStars = Math.floor(rating);
     const hasHalfStar = rating % 1 !== 0;
     const ratingDiv = document.createElement('div');
-    // Agregar estrellas completas
+    ratingDiv.classList.add('star');
+
     for (let i = 0; i < fullStars; i++) {
         const starIcon = document.createElement('i');
         starIcon.classList.add('fas', 'fa-star');
         ratingDiv.appendChild(starIcon);
     }
-    // Agregar media estrella si hay medio punto
+
     if (hasHalfStar) {
         const halfStarIcon = document.createElement('i');
         halfStarIcon.classList.add('fas', 'fa-star-half-alt');
         ratingDiv.appendChild(halfStarIcon);
     }
-    // Agregar estrellas vacías restantes hasta completar 5
+
     const remainingStars = 5 - Math.ceil(rating);
     for (let i = 0; i < remainingStars; i++) {
         const emptyStarIcon = document.createElement('i');
         emptyStarIcon.classList.add('far', 'fa-star');
         ratingDiv.appendChild(emptyStarIcon);
     }
+
     return ratingDiv;
+}
+
+function formatPrice(price) {
+    const current_price = price.current_price;
+    if (current_price) {
+        const formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        });
+        return formatter.format(current_price);
+    }
+    return 'Price not available';
 }
